79c14a42134145a239ea5addbf0db5427c0c2c05
Maciej <maciejgrochowski93@o2.pl>
2021-07-31 14:35:27 +0200

    sending through asynchronous way with WebClient

--- src/main/java/maciej/grochowski/moviecatalogservice/resource/MovieCatalogController.java
+++ src/main/java/maciej/grochowski/moviecatalogservice/resource/MovieCatalogController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +21,9 @@ public class MovieCatalogController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalogOfUser(@PathVariable String userId) {
@@ -35,11 +39,29 @@ public class MovieCatalogController {
 
         // for each movie ID call info service and get details
         // replacing Rating with the CatalogItem
+//        return ratingList
+//                .stream()
+//                .map(rating -> {
+//                    // RestTemplate contains 2 parameters: the link it refers to, and the class of object which is supposed to be recreated
+//                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+//                    return new CatalogItem(movie.getMovieName(), "description", rating.getRating());
+//                })
+//                .collect(Collectors.toList());
+
+
+        // asynchronous
         return ratingList
                 .stream()
                 .map(rating -> {
-                    // RestTemplate contains 2 parameters: the link it refers to, and the class of object which is supposed to be recreated
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    Movie movie = webClientBuilder
+                            // build it, use GET() [could be any other CRUD], give the link and retrieve information.
+                            // change received String into instance of Movie, and then BLOCK until the whole list is sent
+                            .build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
                     return new CatalogItem(movie.getMovieName(), "description", rating.getRating());
                 })
                 .collect(Collectors.toList());
