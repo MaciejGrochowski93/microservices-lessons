5907af06b0c3820004a3cf80dcd34d1cf042c3d8
Maciej <maciejgrochowski93@o2.pl>
2021-08-04 23:44:29 +0200

    wrapping up the first microservices course

--- src/main/java/maciej/grochowski/moviecatalogservice/resource/MovieCatalogController.java
+++ src/main/java/maciej/grochowski/moviecatalogservice/resource/MovieCatalogController.java
@@ -23,14 +23,14 @@ public class MovieCatalogController {
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalogOfUser(@PathVariable String userId) {
 
-        UserRating ratingList = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratingList = restTemplate.getForObject("http://movie-ratings-service/ratingsdata/users/" + userId, UserRating.class);
 
 //         for each movie ID call info service and get detail replacing Rating with the CatalogItem
         return ratingList
                 .getRatingList()
                 .stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getMovieName(), "description", rating.getRating());
                 })
                 .collect(Collectors.toList());
